754098c84f99c2b6429a17a0e82f23608ac339f0
vergilet <yaroslavo@interlink-ua.com>
2015-03-27 13:36:43 +0200

    Update iChar.js

--- iChar.js
+++ iChar.js
@@ -1,11 +1,11 @@
 var iChar = {
-
+  
   sharedKeysBetween: function(firstHash, secondHash){
     var resultingHash = {};
-    for (key in firstHash)
+    for (key in firstHash) {
       if(secondHash[key])
-        resultingHash[key] = secondHash[key];
-    });
+       resultingHash[key] = secondHash[key];
+    }
     return resultingHash;
   },
   
